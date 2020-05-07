77f9c8a80e5de9ec8ac53c9a4a3a125a050d22c1
Charles Jackson <cbj170230@utdallas.edu>
2020-05-07 16:41:50 -0500

    Document hashtable use

--- test.c
+++ test.c
@@ -5,8 +5,10 @@
 #include "hashtable.h"
 
 int main(int argc, char** argv) {
+    /* creating a hashtable named `sloths` */
     newhashtable(sloths, char*, int, 8, hashstr, strequal);
-    puthashnorehash(sloths, "George", 13);
+    /* adding to sloths' names and ages */
+    puthash(sloths, "George", 13);
     puthash(sloths, "Johny", 12);
     puthash(sloths, "Fred", 19);
     puthash(sloths, "Calvin", 14);
@@ -17,11 +19,13 @@ int main(int argc, char** argv) {
     puthash(sloths, "Nancy", 17);
     puthash(sloths, "Dash", 21);
     puthash(sloths, "Logan", 20);
+    /* retriving some sloth's ages and printing them */
     int age;
     gethash(sloths, "George", age);
     printf("George is %d years old\n", age);
     gethash(sloths, "Dash", age);
     printf("Dash is %d years old\n", age);
+    /* deleteing the hashtable named `sloths` */
     deletehashtable(sloths, free, free);
 }
 
